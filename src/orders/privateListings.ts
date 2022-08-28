3dd600c97cdb7b74c7162204faffeafc1b2f1a48
李长青 <qing@freelink.cc>
2022-08-28 18:28:42 +0800

    202208281828

--- src/orders/privateListings.ts
+++ src/orders/privateListings.ts
@@ -4,9 +4,9 @@ import {
   MatchOrdersFulfillment,
   Order,
   OrderWithCounter,
-} from "@opensea/seaport-js/lib/types";
-import { isCurrencyItem } from "@opensea/seaport-js/lib/utils/item";
-import { generateRandomSalt } from "@opensea/seaport-js/lib/utils/order";
+} from "@opensea/seaport-js-gas/lib/types";
+import { isCurrencyItem } from "@opensea/seaport-js-gas/lib/utils/item";
+import { generateRandomSalt } from "@opensea/seaport-js-gas/lib/utils/order";
 import { BigNumber } from "ethers";
 
 export const getPrivateListingConsiderations = (
--- src/sdk.ts
+++ src/sdk.ts
@@ -1,10 +1,10 @@
-import { Seaport } from "seaport-js";
-import { CROSS_CHAIN_SEAPORT_ADDRESS } from "seaport-js/lib/constants";
+import { Seaport } from "@opensea/seaport-js-gas";
+import { CROSS_CHAIN_SEAPORT_ADDRESS } from "@opensea/seaport-js-gas/lib/constants";
 import {
   ConsiderationInputItem,
   CreateInputItem,
   OrderComponents,
-} from "seaport-js/lib/types";
+} from "@opensea/seaport-js-gas/lib/types";
 import { BigNumber } from "bignumber.js";
 import { Web3JsProvider } from "ethereum-types";
 import { isValidAddress } from "ethereumjs-util";
